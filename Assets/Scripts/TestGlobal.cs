92de766abb4b7bf3b50bd9384b5d9b762acd8957
liuzeliang <434421502@qq.com>
2018-12-12 23:05:15 +0800

    first commit

---
+++ Assets/Scripts/TestGlobal.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestGlobal : MonoBehaviour{
+	void onStart()
+	{}
+}
+public class Global {
+
+	static public void TestFunc()
+	{
+		Debug.Log("lsllslsl");
+	}
+}
